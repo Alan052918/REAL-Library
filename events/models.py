9f1c1003400b5af7b53a4310f9160667f2743ce8
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2022-12-09 20:02:45 -0500

    Alter Sponsor model

--- events/models.py
+++ events/models.py
@@ -25,6 +25,7 @@ class Exhibition(Event):
 
 
 class Sponsor(models.Model):
+    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30, null=True, blank=True)
     type = models.CharField(max_length=1, validators=[MinLengthValidator(1)])
